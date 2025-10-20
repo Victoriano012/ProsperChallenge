224698cd43dafed9a3df12746138579fa4685f17
Victor Conchello Vendrell <victorconchello@gmail.com>
2025-10-20 19:05:59 +0200

    Local bot testing

--- bot.py
+++ bot.py
@@ -24,12 +24,18 @@ Run the bot using::
 
 import os
 
+from pipecat.audio.vad.vad_analyzer import VADParams
+from pipecat.transports.daily.transport import DailyParams
+
 from utils import get_system_prompt, get_tools
 from pipecat.processors.aggregators.llm_context import LLMContext
 from pipecat.adapters.schemas.tools_schema import ToolsSchema
 from pipecat.processors.aggregators.llm_response_universal import (
     LLMContextAggregatorPair,
 )
+from pipecat.runner.utils import create_transport
+from pipecat.audio.turn.smart_turn.local_smart_turn_v3 import LocalSmartTurnAnalyzerV3
+from pipecat.transports.base_transport import TransportParams
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -92,7 +98,7 @@ async def run_bot(transport: BaseTransport):
     )
     # stt = OpenAISTTService(api_key=OPENAI_API_KEY)
     # tts = OpenAITTSService(api_key=OPENAI_API_KEY)
-    llm = OpenAILLMService(api_key=OPENAI_API_KEY, model="gpt-4.1-mini")
+    llm = OpenAILLMService(api_key=OPENAI_API_KEY, model="gpt-4.1")
     rtvi = RTVIProcessor(config=RTVIConfig(config=[]))
 
     # Set up the initial LLM context with a system prompt and tools.
@@ -118,8 +124,6 @@ async def run_bot(transport: BaseTransport):
     task = PipelineTask(
         pipeline,
         params=PipelineParams(
-            audio_in_sample_rate=8000,
-            audio_out_sample_rate=8000,
             enable_metrics=True,
             enable_usage_metrics=True,
         ),
@@ -130,9 +134,6 @@ async def run_bot(transport: BaseTransport):
     async def on_client_connected(transport, client):
         logger.info(f"Client connected")
         # Kick off the conversation.
-        # messages.append(
-        #     {"role": "system", "content": "Say hello and briefly introduce yourself."}
-        # )
         await task.queue_frame(LLMRunFrame())
 
     @transport.event_handler("on_client_disconnected")
@@ -148,26 +149,24 @@ async def run_bot(transport: BaseTransport):
 async def bot(runner_args: RunnerArguments):
     """Main bot entry point for the bot starter."""
 
-    transport_type, call_data = await parse_telephony_websocket(runner_args.websocket)
-    logger.info(f"Auto-detected transport: {transport_type}")
-
-    serializer = TwilioFrameSerializer(
-        stream_sid=call_data["stream_id"],
-        call_sid=call_data["call_id"],
-        account_sid=os.getenv("TWILIO_ACCOUNT_SID", ""),
-        auth_token=os.getenv("TWILIO_AUTH_TOKEN", ""),
-    )
-
-    transport = FastAPIWebsocketTransport(
-        websocket=runner_args.websocket,
-        params=FastAPIWebsocketParams(
-            audio_in_enabled=True,
-            audio_out_enabled=True,
-            add_wav_header=False,
-            vad_analyzer=SileroVADAnalyzer(),
-            serializer=serializer,
+    common_transport_params = {
+        "audio_in_enabled": True,
+        "audio_out_enabled": True,
+        "vad_analyzer": SileroVADAnalyzer(
+            params=VADParams(stop_secs=0.3, start_secs=0.0)
         ),
-    )
+    }
+    transport_params = {
+        "webrtc": lambda: TransportParams(
+            **common_transport_params,
+            turn_analyzer=LocalSmartTurnAnalyzerV3(),
+        ),
+        "twilio": lambda: FastAPIWebsocketParams(
+            **common_transport_params,
+            add_wav_header=False,
+        ),
+    }
+    transport = await create_transport(runner_args, transport_params)
 
     await run_bot(transport)
 
